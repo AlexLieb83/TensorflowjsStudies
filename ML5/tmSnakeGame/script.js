8a28649d8d477d12b5ec941b7a6fabe7a77fc1ec
alexlieb <alexliebhaber1992@gmail.com>
2022-12-27 14:22:45 -0600

    teachable machine snake game project

---
+++ ML5/tmSnakeGame/script.js
@@ -0,0 +1,171 @@
+export default class Snake {
+  constructor() {
+    this.body = [];
+    this.body[0] = createVector(floor(w / 2), floor(h / 2));
+    this.xdir = 0;
+    this.ydir = 0;
+    this.len = 0;
+  }
+
+  setDir(x, y) {
+    this.xdir = x;
+    this.ydir = y;
+  }
+
+  update() {
+    let head = this.body[this.body.length - 1].copy();
+    this.body.shift();
+    head.x += this.xdir;
+    head.y += this.ydir;
+    this.body.push(head);
+  }
+
+  grow() {
+    let head = this.body[this.body.length - 1].copy();
+    this.len++;
+    this.body.push(head);
+  }
+
+  endGame() {
+    let x = this.body[this.body.length - 1].x;
+    let y = this.body[this.body.length - 1].y;
+    if (x > w - 1 || x < 0 || y > h - 1 || y < 0) {
+      return true;
+    }
+    for (let i = 0; i < this.body.length - 1; i++) {
+      let part = this.body[i];
+      if (part.x == x && part.y == y) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  eat(pos) {
+    let x = this.body[this.body.length - 1].x;
+    let y = this.body[this.body.length - 1].y;
+    if (x == pos.x && y == pos.y) {
+      this.grow();
+      return true;
+    }
+    return false;
+  }
+
+  show() {
+    for (let i = 0; i < this.body.length; i++) {
+      fill(0);
+      noStroke();
+      rect(this.body[i].x, this.body[i].y, 1, 1);
+    }
+  }
+}
+
+// The video
+let video;
+let flipVideo;
+
+// Storing the label
+let label = "waiting...";
+
+// The classifier
+let classifier;
+let modelURL = "https://teachablemachine.withgoogle.com/models/zyZYGw6QU/";
+
+// STEP 1: Load the model!
+function preload() {
+  classifier = ml5.imageClassifier(modelURL + "model.json");
+}
+
+// Snake Game Variables
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
+
+function setup() {
+  createCanvas(640, 480);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+  // Mirro the video since we trained it that way!
+  flipVideo = ml5.flipImage(video);
+
+  // STEP 2: Start classifying
+  classifyVideo();
+
+  // Snake Game
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(5);
+  snake = new Snake();
+  foodLocation();
+}
+
+// STEP 2 classify!
+function classifyVideo() {
+  // Flip the video!
+  flipVideo = ml5.flipImage(video);
+  classifier.classify(flipVideo, gotResults);
+}
+
+// Snake Game
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+}
+
+// Control the game based on the label
+function controlSnake() {
+  if (label === "left") {
+    snake.setDir(-1, 0);
+  } else if (label === "right") {
+    snake.setDir(1, 0);
+  } else if (label === "down") {
+    snake.setDir(0, 1);
+  } else if (label === "up") {
+    snake.setDir(0, -1);
+  }
+}
+
+function draw() {
+  background(255);
+
+  // Draw the video?
+  image(flipVideo, 0, 0);
+  textSize(32);
+  fill(0);
+  text(label, 10, 50);
+
+  // Draw the game
+  scale(rez);
+  if (snake.eat(food)) {
+    foodLocation();
+  }
+  snake.update();
+  snake.show();
+
+  if (snake.endGame()) {
+    print("END GAME");
+    background(255, 0, 0);
+    noLoop();
+  }
+
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
+}
+
+// STEP 3: Get the classification!
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  label = results[0].label;
+  // Control the snake and classify again!
+  controlSnake();
+  classifyVideo();
+}
